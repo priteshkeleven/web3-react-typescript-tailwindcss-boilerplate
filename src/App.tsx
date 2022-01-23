bc215669163119caf0a46fe6f4a7f94c9959c544
Pritesh K <37466934+priteshkeleven@users.noreply.github.com>
2022-01-23 21:47:51 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, Fragment } from "react";
+import { useWeb3React } from "@web3-react/core";
+import { MetaMask } from "./connectors";
+import Bignumber from "bignumber.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+
+const App = () => {
+    const { active, account, activate, library } = useWeb3React();
+    const [blockNumber, setBlockNumber] = useState(null);
+    const [network, setNetwork] = useState<any>(null);
+    const [balance, setBalance] = useState<any>(null);
+
+    const handleClick = async () => {
+        if (!active) {
+            activate(MetaMask);
+        }
+    };
+
+    useEffect(() => {
+        if (!active) {
+            activate(MetaMask);
+        }
+
+        if (active) {
+            library.getBlockNumber().then(setBlockNumber);
+            library.getNetwork().then(setNetwork);
+            library.getBalance(account).then(setBalance);
+        }
+    }, [active, library, activate, account]);
+
+    console.log();
+
+    return (
+        <Fragment>
+            <h1 className='text-3xl font-bold text-center'>Hello world!!!</h1>
+
+            <div className='text-center mt-3'>
+                <button
+                    className='bg-slate-400 px-3 py-2 rounded-md text-white'
+                    onClick={handleClick}
+                >
+                    {!active ? "connect" : "disconnect"}
+                </button>
+            </div>
+
+            <p className='text-lg text-center mt-5 font-mono'>
+                Account: {account}<br />
+                Balance: {balance ? new Bignumber(balance.toString()).dividedBy(1e18).toString() : "0"}<br />
+                Block Number: {blockNumber}<br />
+                Chain: {network?.name} ({network?.chainId})
+            </p>
+        </Fragment>
+    );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Web3ReactProvider } from '@web3-react/core';
+import { ethers } from 'ethers';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const getLibrary = (provider: ethers.providers.ExternalProvider) => {
+    return new ethers.providers.Web3Provider(provider) // this will vary according to whether you use e.g. ethers or web3.js
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Web3ReactProvider getLibrary={getLibrary}>
+            <App />
+        </Web3ReactProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
